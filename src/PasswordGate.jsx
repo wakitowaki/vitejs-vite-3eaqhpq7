1762ebb198ce413f0d38ce26b39b7dca843efac2
wakitowaki <matteo.sirizzotti@gmail.com>
2025-04-26 01:37:22 +0200

    Update PasswordGate.jsx

--- src/PasswordGate.jsx
+++ src/PasswordGate.jsx
@@ -56,7 +56,7 @@ export default function PasswordGate({ children }) {
         <div
             className="relative min-h-screen text-gray-800 font-sans px-4 py-8"
             style={{
-                backgroundImage: 'url(https://images.unsplash.com/photo-1470290378698-263fa07bba56?ixlib=rb-4.0.3&auto=format&fit=crop&w=1470&q=80)',
+                backgroundImage: 'url(https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmVidWxhfGVufDB8fDB8fA%3D%3D&ixlib=rb-4.0.3&w=1920&q=80)',
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
                 backgroundAttachment: 'fixed',
